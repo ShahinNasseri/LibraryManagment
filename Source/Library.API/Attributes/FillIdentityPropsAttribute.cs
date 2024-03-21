fa2e2c8b78090e8b68a6c1785e566c8ac7f8e2a8
ShahinNasseri <shahin.nk.1995@gmail.com>
2024-03-21 15:16:51 +0330

    Add FillIdentityPropFiller

---
+++ Source/Library.API/Attributes/FillIdentityPropsAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Library.API.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Library.API.Attributes
+{
+    public class FillIdentityPropsAttribute : ActionFilterAttribute
+    {
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var user = (context.HttpContext.User as UserPrincipal);
+            if (user != null)
+            {
+                var requestModel = context.ActionArguments.Values.FirstOrDefault();
+
+                var modelType = requestModel?.GetType();
+                var props = modelType?.GetProperties();
+                if (modelType != null && props != null)
+                {
+                    foreach (var prop in props)
+                    {
+                        if (prop.Name.ToLower() == "userid")
+                        {
+                            prop.SetValue(requestModel, user.UserId);
+                        }
+                    }
+
+                    context.ActionArguments[key: context.ActionArguments.Keys.FirstOrDefault()] = requestModel;
+                }
+
+            }
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
---
+++ Source/Library.API/Models/UserPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+
+namespace Library.API.Models
+{
+    public class UserPrincipal : ClaimsPrincipal
+    {
+        public UserPrincipal(ClaimsIdentity identity) : base(identity) { }
+
+        public UserPrincipal() { }
+
+        /// <summary>
+        /// User Id
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+
+
+        /// <summary>
+        /// FirstName of user
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// FirstName of user
+        /// </summary>
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// Email of user
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
