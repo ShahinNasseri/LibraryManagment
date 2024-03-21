23ff67d6a1bba71be412df213eddca548870bddb
ShahinNasseri <shahin.nk.1995@gmail.com>
2024-03-21 23:39:05 +0330

    Update FillIdentityPropsAttribute And Add Admin Proncipal

---
+++ Source/Library.API/Models/AdminPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+
+namespace Library.API.Models
+{
+    public class AdminPrincipal: ClaimsPrincipal
+    {
+        public AdminPrincipal(ClaimsIdentity identity) : base(identity) { }
+
+        public AdminPrincipal() { }
+
+        /// <summary>
+        /// Admin Id
+        /// </summary>
+        public long AdminId { get; set; }
+
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Email of user
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
