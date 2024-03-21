9df868fe046aa7167d1a00d31ad5713a3bd3896d
ShahinNasseri <shahin.nk.1995@gmail.com>
2024-03-21 15:17:15 +0330

    Add ApiResult And ControllerBase

---
+++ Source/Library.API/Controllers/AdminBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Library.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AdminBaseController : ControllerBase
+    {
+        protected ApiResult Response;
+        public AdminBaseController()
+        {
+            Response = new ApiResult(null);
+        }
+    }
+}
---
+++ Source/Library.API/Models/ApiResult.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Library.API.Models
+{
+    public class ApiResult : IActionResult
+    {
+        [JsonPropertyName("errors")]
+        public string[] Errors { get; set; }
+
+        [JsonPropertyName("data")]
+        public object? Data { get; set; }
+
+        [JsonPropertyName("statusCode")]
+        public int? StatusCode { get; set; }
+
+        public ApiResult(object? data, int statusCode = StatusCodes.Status200OK, string[] errors = null)
+        {
+            Data = data;
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            await new OkObjectResult(this).ExecuteResultAsync(context);
+        }
+
+    }
+
+    public class ApiResult<T> : IActionResult
+    {
+        [JsonPropertyName("errors")]
+        public string[] Errors { get; set; }
+
+        [JsonPropertyName("data")]
+        public List<T> Data { get; set; }
+
+        [JsonPropertyName("statusCode")]
+        public int? StatusCode { get; set; }
+
+        public ApiResult(List<T> data, int statusCode = StatusCodes.Status200OK, string[] errors = null)
+        {
+            Data = data;
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            await new OkObjectResult(this).ExecuteResultAsync(context);
+        }
+
+    }
+    public class ApiResult<T, TT> : IActionResult
+    {
+        [JsonPropertyName("errors")]
+        public string[] Errors { get; set; }
+
+        [JsonPropertyName("data")]
+        public T Data { get; set; }
+
+        [JsonPropertyName("statusCode")]
+        public int? StatusCode { get; set; }
+
+        public ApiResult(T data, int statusCode = StatusCodes.Status200OK, string[] errors = null)
+        {
+            Data = data;
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            await new OkObjectResult(this).ExecuteResultAsync(context);
+        }
+
+    }
+}
